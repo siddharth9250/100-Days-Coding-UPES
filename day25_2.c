90f5d151d59eb9d7e2326954b1d5d96428b7f0cc
siddharth9250 <siddharthakushwaha006@gmail.com>
2026-09-03 13:56:28 +0530

    Add files via upload

---
+++ day25_2.c
@@ -0,0 +1,29 @@
+/*Q50: Write a program to print the following pattern:
+*****
+ ****
+  ***
+   **
+    *  */
+   #include <stdio.h>
+
+int main() {
+    int i, j;
+
+    for (i = 1; i <= 5; i++) {
+
+        // Print spaces
+        for (j = 1; j < i; j++) {
+            printf(" ");
+        }
+
+        // Print stars
+        for (j = i; j <= 5; j++) {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
+
